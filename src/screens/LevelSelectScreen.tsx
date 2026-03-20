dbf95d94873ca846e0fd41242250e70db810514a
koushik12019976 <84120351+koushikm8926@users.noreply.github.com>
2026-03-20 14:08:14 +0530

    feat: add a header image to the LevelSelectScreen with new styling.

--- src/screens/LevelSelectScreen.tsx
+++ src/screens/LevelSelectScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface Props {
@@ -13,7 +13,16 @@ export default function LevelSelectScreen({ navigation }: Props) {
       style={styles.container}
       resizeMode="cover"
     >
-      <SafeAreaView style={styles.safeArea} edges={['top']} />
+      <SafeAreaView style={styles.safeArea} edges={['top']}>
+        <View style={styles.headerContainer}>
+          <Image 
+            source={require('../../level-header.png')}
+            style={styles.headerImage}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={{ flex: 1 }} />
+      </SafeAreaView>
     </ImageBackground>
   );
 }
@@ -25,6 +34,16 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
   },
+  headerContainer: {
+    paddingTop: 40,
+    alignItems: 'center',
+    width: '100%',
+  },
+  headerImage: {
+    width: '75%',
+    height: 75,
+  },
 });
+
 
 
